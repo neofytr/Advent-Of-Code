b951e0418805264687b989c155b192df3da4e663
RajDaBest <imagination12344321.com@gmail.com>
2024-12-25 18:39:53 +0530

    starting day eight

---
+++ Dec/DayEight/main.c
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "./Strix/header/strix.h"
+
+int main()
+{
+    FILE *input_file = fopen("input.txt", "r");
+    if (!input_file)
+    {
+        fprintf(stderr, "Failed to open input file\n");
+        return EXIT_FAILURE;
+    }
+
+    int input_file_len = fseek(input_file, 0, SEEK_END);
+    if (input_file_len < 0)
+    {
+        fprintf(stderr, "Failed to seek to end of file\n");
+        fclose(input_file);
+        return EXIT_FAILURE;
+    }
+
+    input_file_len = (int)ftell(input_file);
+    if (input_file_len < 0)
+    {
+        fprintf(stderr, "Failed to get file length\n");
+        fclose(input_file);
+        return EXIT_FAILURE;
+    }
+
+    if (fseek(input_file, 0, SEEK_SET) < 0)
+    {
+        fprintf(stderr, "Failed to seek to start of file\n");
+        fclose(input_file);
+        return EXIT_FAILURE;
+    }
+
+    uint8_t *input_file_array = (uint8_t *)malloc((input_file_len + 1) * sizeof(uint8_t));
+    if (!input_file_array)
+    {
+        fprintf(stderr, "Failed to allocate memory for file content\n");
+        fclose(input_file);
+        return EXIT_FAILURE;
+    }
+    input_file_array[input_file_len] = '\0';
+
+    size_t bytes_read = fread((void *)input_file_array, sizeof(uint8_t), input_file_len, input_file);
+    if (bytes_read < (size_t)(input_file_len / sizeof(uint8_t)))
+    {
+        fprintf(stderr, "Failed to read file content\n");
+        fclose(input_file);
+        free(input_file_array);
+        return EXIT_FAILURE;
+    }
+
+    strix_t *input_strix = strix_create((const char *)input_file_array);
+    if (!input_strix)
+    {
+        fprintf(stderr, "Failed to create input strix\n");
+        fclose(input_file);
+        free(input_file_array);
+        return EXIT_FAILURE;
+    }
+    free(input_file_array);
+    fclose(input_file);
+
+}
